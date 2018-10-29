3cfa0811e5d7afdf3bbccc1de5e957563b46faf1
Greg <gregory.kenenitz@gmail.com>
2018-10-29 19:27:50 -0400

    Creates search functionality for serial_number of devices

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -8,6 +8,10 @@ class DevicesController < ApplicationController
   end
 
   def index
-    @devices = Device.all
+    @devices = Device.search(params[:serial_number])
+  end
+
+  private def devices_params
+    params.require(:device).permit(:serial_number, :registration_date, :firmware_version, :search)
   end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,12 @@
 class Device < ApplicationRecord
   has_many :sensor_readings
+  
+  def self.search(search)
+    if search
+      results = where(serial_number: search)
+      results.empty? ? Device.all : results
+    else
+      Device.all
+    end
+  end
 end
