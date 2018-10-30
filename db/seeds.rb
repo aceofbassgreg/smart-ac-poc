092fcb051ac8963243428d4400169df6199152ae
Greg <gregory.kenenitz@gmail.com>
2018-10-29 21:56:23 -0400

    removes seeds comments, makes temperatures make sense for celsius

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 chars = ('A'..'Z').to_a + (1..9).to_a
 registration_dates = [Date.today, Date.yesterday, Date.yesterday - 1]
-temperatures = (68..75).to_a
+temperatures = (19..24).to_a
 co2_values = (1..6).step(0.5).to_a
 air_humidity_percentages = (50..65).step(0.5).to_a
 device_health = ['Healthy', 'Not Too Healthy', 'OK']
