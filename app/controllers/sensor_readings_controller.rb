dd486f418acd808b39a352df046007b49aa3261e
Greg <gregory.kenenitz@gmail.com>
2018-10-29 21:23:27 -0400

    displays sensor readings for given device on separate chart

--- app/controllers/sensor_readings_controller.rb
+++ app/controllers/sensor_readings_controller.rb
@@ -3,8 +3,18 @@ class SensorReadingsController < ApplicationController
   end
 
   def index
+    device_id = params[:device_id]
+    @device = Device.find(device_id)
+    @sensor_readings = SensorReading.search(params[:device_id])
   end
 
   def show
+  end
+
+  private def sensor_readings_params
+    params.require(:sensor_reading).permit(
+      :temperature, :carbon_monoxide_level, :air_humidity_percentage,
+      :device_health, :device_id
+    )
   end
 end
--- app/models/sensor_reading.rb
+++ app/models/sensor_reading.rb
@@ -1,3 +1,7 @@
 class SensorReading < ApplicationRecord
   belongs_to :device
+
+  def self.search(search)
+    where(device_id: search)
+  end
 end
