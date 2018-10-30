7e56dda32e5da1f015a0b7a457ef85e42adc438c
Greg <gregory.kenenitz@gmail.com>
2018-10-30 08:41:27 -0400

    Adds jquery-rails, checks in charts assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require semantic-ui
+//= require Chart.bundle
+//= require chartkick
 //= require_tree .
