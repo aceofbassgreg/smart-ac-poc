4ed90c3dae4a04b21d783332f9307720ff8558ce
Greg <gregory.kenenitz@gmail.com>
2018-10-29 21:38:37 -0400

    Adds seed data to have something to work with while tweaking views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+chars = ('A'..'Z').to_a + (1..9).to_a
+dates = [Date.today, Date.yesterday, Date.yesterday - 1]
+temperatures = (68..75).to_a
+co2_values = (1..6).step(0.5).to_a
+air_humidity_percentages = (50..65).step(0.5).to_a
+device_health = ['Healthy', 'Not Too Healthy', 'OK']
+
+30.times do 
+  device = Device.create!(
+    serial_number: chars.shuffle[0..14].join(''),
+    registration_date: dates.sample,
+    firmware_version: 1
+  )
+  10.times do
+    device.sensor_readings.create(
+      temperature: temperatures.sample,
+      carbon_monoxide_level: co2_values.sample,
+      air_humidity_percentage: air_humidity_percentages.sample,
+      device_health: device_health.sample
+    )
+  end
+end
