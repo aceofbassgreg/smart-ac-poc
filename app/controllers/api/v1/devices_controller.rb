ae16b8e7e7d90ed408461d0a48d726dd75bb957a
Greg <gregory.kenenitz@gmail.com>
2018-11-01 17:10:22 -0400

    replaces error handling for not null with strong param requirement

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -4,7 +4,7 @@ module Api
       skip_before_action :verify_authenticity_token
 
       rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
-        response = { reason: 'required parameter omitted', 'details': 'payload must include "serial_number"' }
+        response = { reason: 'required parameter omitted', 'details': 'payload must include "serial_number" and "firmware_version"' }
         respond_to do |format|
           format.json { render json: response, status: :unprocessable_entity }
         end
@@ -13,9 +13,6 @@ module Api
       def create
         device = Device.create(device_params)
         render json: device, status: 200
-      rescue ActiveRecord::NotNullViolation => e
-        details = e.message.gsub('PG::NotNullViolation: ERROR:  ','').gsub(/\n.+/,'')
-        render json: {'reason': 'required field omitted', 'details': details}, status: 422
       rescue ActiveRecord::RecordNotUnique
         render json: {'reason': "serial number #{device_params[:serial_number]} is already registered", 'details': ''}, status: 422
       end
@@ -23,6 +20,7 @@ module Api
       def device_params
         params.require('device').permit(:serial_number, :firmware_version).tap do |device_attrs|
           device_attrs.require(:serial_number)
+          device_attrs.require(:firmware_version)
         end
       end
     end
