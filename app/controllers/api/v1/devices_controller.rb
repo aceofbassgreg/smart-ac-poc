432f784a1ef81209acba4b49882b8d9918889eb6
Greg <gregory.kenenitz@gmail.com>
2018-11-01 14:40:37 -0400

    Can now create device through devices controller, returns 200 whether new device created or not for now

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -4,7 +4,12 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def create
-        puts "SUCCESS"
+        device = Device.create(device_params)
+        render json: device, status: 200
+      end
+
+      def device_params
+        params.require('device').permit(:serial_number, :firmware_version)
       end
     end
   end
