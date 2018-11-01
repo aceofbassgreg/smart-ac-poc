225a3039bf330ad6d00dbf9dfdd7181bfd7a57c2
Greg <gregory.kenenitz@gmail.com>
2018-11-01 15:26:54 -0400

    Successfully saves SensorReading from API POST, associates with proper device

--- app/controllers/api/v1/sensor_readings_controller.rb
+++ app/controllers/api/v1/sensor_readings_controller.rb
@@ -4,11 +4,18 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def create
-        puts params
+        device_serial_number = sensor_reading_params[:device_attributes][:serial_number]
+        device = Device.find_by(serial_number: device_serial_number)
+        attrs = sensor_reading_params.except(:device_attributes)
+        sensor_reading = SensorReading.create(attrs.merge(device_id: device.id))
+        render json: sensor_reading, status: 200
       end
 
       def sensor_reading_params
-        params.require('sensor_reading').permit(:carbon_monoxide_level, :air_humidity_percentage, :temperature)
+        params.require('sensor_reading').permit(
+          :carbon_monoxide_level, :air_humidity_percentage, :temperature, [
+            device_attributes: [:serial_number]]
+        )
       end
     end
   end
