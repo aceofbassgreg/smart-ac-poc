a2c148967c9b674ff792967fa418f9e5449aa21a
Greg <gregory.kenenitz@gmail.com>
2018-11-01 22:16:38 -0400

    moves new API logic to v2 dir

---
+++ app/controllers/api/v2/devices_controller.rb
@@ -0,0 +1,34 @@
+require 'securerandom'
+
+module Api
+  module V1
+    class DevicesController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
+      rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+        response = { reason: 'required parameter omitted', 'details': 'payload must include "serial_number" and "firmware_version"' }
+        respond_to do |format|
+          format.json { render json: response, status: :unprocessable_entity }
+        end
+      end
+
+      def create
+        serial_number = device_params[:serial_number]
+        # Make fake email account so we can register device as user, return token upon registration
+        # TODO:  maybe email would have some value? maybe we can return email password to device as well?
+        user = User.create(email: "#{serial_number}@smart_ac_company.com", password: SecureRandom.hex)
+        device = Device.create(device_params)
+        render json: user.api_authentication_key, status: 200
+      rescue ActiveRecord::RecordNotUnique
+        render json: {'reason': "serial number #{device_params[:serial_number]} is already registered", 'details': ''}, status: 422
+      end
+
+      def device_params
+        params.require('device').permit(:serial_number, :firmware_version).tap do |device_attrs|
+          device_attrs.require(:serial_number)
+          device_attrs.require(:firmware_version)
+        end
+      end
+    end
+  end
+end
---
+++ app/controllers/api/v2/sensor_readings_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class SensorReadingsController < ApplicationController
+
+      include Authenticator
+
+      skip_before_action :verify_authenticity_token
+      before_action :authenticate_api_user
+
+      rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+        response = { reason: 'required parameter omitted', 'details': 'payload must include: {"device_attributes": {"serial_number": "SERIAL_NUMBER"}} and must have ANY of the following keys:  tmeperature, carbon_monoxide_level, device_health, air_humidity_percentage. No other parameteres  are permitted' }
+        respond_to do |format|
+          format.json { render json: response, status: :unprocessable_entity }
+        end
+      end
+
+      def create
+        unless (sensor_reading_params.dig(:carbon_monoxide_level) || 
+          sensor_reading_params.dig(:air_humidity_percentage) || 
+          sensor_reading_params.dig(:temperature) || 
+          sensor_reading_params.dig(:system_health)
+        )
+          render json: {'reason': 'no sensor data sent', 'details': ''}, status: 400
+          return
+        end
+        sensor_reading = SensorReading.create(sensor_reading_params.merge(device_id: device.id))
+        render json: sensor_reading, status: 200
+      end
+
+      private def device
+        Device.find_by(serial_number: device_serial_number)
+      end
+
+      private def device_serial_number
+        current_user.email.split('@').first
+      end
+
+      private def sensor_reading_params
+        params.require('sensor_reading').permit(
+          :carbon_monoxide_level, :air_humidity_percentage, :temperature, :device_health, :time_recorded
+        )
+      end
+    end
+  end
+end
