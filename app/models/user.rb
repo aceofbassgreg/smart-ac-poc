7f291e6c9265c93578ceac472244363528a39b68
Greg <gregory.kenenitz@gmail.com>
2018-11-01 23:02:19 -0400

    BUG FIX:  allow all caps in devise email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   def api_authentication_key
     Base64.urlsafe_encode64("#{email}:#{api_access_token}")
   end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
