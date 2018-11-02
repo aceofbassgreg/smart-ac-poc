dad46c7bd766ca9abeb911ba9d8d5f2ac36eb03e
Greg <gregory.kenenitz@gmail.com>
2018-11-02 02:09:05 -0400

    more nuanced handling of partial and complete failures

--- app/controllers/api/v2/batch_sensor_readings_controller.rb
+++ app/controllers/api/v2/batch_sensor_readings_controller.rb
@@ -17,8 +17,10 @@ module Api
 
       def create
         errors = []
+        success = []
         batch_sensor_reading_params.each do |sensor_reading|
           if build_sensor_reading_and_handle_response(sensor_reading)
+            success << sensor_reading
             next
           else
             errors << sensor_reading
@@ -26,9 +28,11 @@ module Api
         end
 
         if errors.empty?
-          render json: {'records_created': batch_sensor_reading_params.count}, status: 200
+          render json: {'records_created': success.count}, status: 200
+        elsif success.empty?
+          render json: {'reason': 'no valid records sent', 'details': errors, status: 422}
         else
-          render json: {'reason': 'invalid sensor_readings sent', 'details': errors}, status: 200
+          render json: {'records_created': success.count, 'error_count': errors.count, 'reason': 'invalid sensor_readings sent', 'details': errors}, status: 200
         end
       end
 
