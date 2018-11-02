6527aa02c593609126378acb455b96c25f49a940
Greg <gregory.kenenitz@gmail.com>
2018-11-01 21:28:41 -0400

    declares has_secure_token for user, defines #api_authentication_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_secure_token :api_access_token
+
+  def api_authentication_key
+    Base64.urlsafe_encode64("#{email}:#{api_access_token}")
+  end
 end
